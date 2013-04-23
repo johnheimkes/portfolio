a429b44dcffb6ed3cdfeb5f093f53f39ceee592a
John Heimkes IV <john@markupisart.com>
2013-04-23 18:41:09 -0500

    added a fucking adorable kitten and the loop for the relationship field

---
+++ content/themes/twentytwelve-child/footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve_Child
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+            <p>&copy; 2013 John Heimkes IV &ndash; Built with four generations of love.</p>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
