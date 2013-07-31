8c307975813d133aa57b55cc6192864a7f79ddf0
John Heimkes IV <john@markupisart.com>
2013-07-30 19:33:40 -0500

    added image size for homepage

---
+++ content/themes/twentytwelve-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Child theme functions and definitions.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve_Child
+ */
+
+/**
+ * Add image sizes for post thumbnails
+ */
+add_image_size( 'work-thumb', 360, 166, true );
